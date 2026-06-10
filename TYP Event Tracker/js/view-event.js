013fbcee511178f57279b72e74df9c21ff4bb97b
Han-Yang <hyzhang121@gmail.com>
2026-06-10 10:36:05 -0700

    Profile button checks whether you are logged in or not

---
+++ TYP Event Tracker/js/view-event.js
@@ -0,0 +1,12 @@
+const profileButton = document.querySelector("#profileButton")
+let logInBoolean = null
+const profileButtonLink = document.querySelector("#profileButtonLink")
+
+profileButton.addEventListener('click', () => {
+    logInBoolean = prompt("are you logged in?", "type true or false")
+    if (logInBoolean === "true") {
+        profileButtonLink.href = 'account-in.html'
+    } else {
+        profileButtonLink.href = 'account-out.html'
+    }
+})
