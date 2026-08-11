f88df68c82db9978bf02d6a77527df1e5e3ea19d
Han-Yang <hyzhang121@gmail.com>
2026-08-11 15:43:32 -0700

    restore create event js

--- js/create-event.js
+++ js/create-event.js
@@ -1,16 +1,17 @@
-if (!localStorage.getItem('didRun')) { 
-  let eventArray = []; 
-  for (let i = 0; i < 1; i++) { 
-    eventArray[i] = []; 
-    for (let j = 0; j < 5; j++) { 
-      eventArray[i][j] = 'Placeholder Event'; 
-    } 
-  } 
-  console.log(eventArray); 
-  localStorage.setItem('eventStorage', JSON.stringify(eventArray)); 
-  localStorage.setItem('didRun', 'true'); 
-}
+if (!localStorage.getItem('didRun')) {
+    let eventArray = []
 
+    for (let i=0; i<1; i++) {
+        eventArray[i] = []
+        for(let j = 0; j < 5; j++) {
+            eventArray[i][0] = 'Placeholder Event'
+        }
+    }   
+    console.log(eventArray)
+    localStorage.setItem('eventStorage', JSON.stringify(eventArray))
+
+  localStorage.setItem('didRun', 'true');
+}
 
 
 let eventCount = 0;
@@ -19,14 +20,31 @@ function newEvent(){
     const title = document.getElementById("title").value;
     const date = document.getElementById("date").value;
     const time = document.getElementById('time').value;
-    const location = document.getElementById('location').value;
     const description = document.getElementById("description").value;
     const image = document.getElementById("image").files[0];
 
+    const title =
+        document.getElementById("title").value;
+
+    const date =
+        document.getElementById("date").value;
+    
+    const time =
+        document.getElementById('time').value;
+
+    const description =
+        document.getElementById("description").value;
+
+    const image =
+        document.getElementById("image").files[0];
+    const userInfo = 
+        JSON.parse(localStorage.getItem('userInformation'))
+    const eventCreator =
+        userInfo[0]
     const userInfo = JSON.parse(localStorage.getItem('userInformation'))
     const eventCreator = userInfo[0]
 
-    if(!title || !date || !time || !location){
+    if(!title || !date || !time){
         alert("Please fill out all required fields.");
         return;
     }
@@ -36,6 +54,11 @@ function newEvent(){
         return;
     }
 
+    const retreivedData = JSON.parse(localStorage.getItem('eventStorage'))
+    retreivedData.push([title, date, time, description, image, eventCreator])
+    console.log(retreivedData)
+    localStorage.setItem('eventStorage', JSON.stringify(retreivedData))
+}
     function saveToStorage(image) {
         const retreivedData = JSON.parse(localStorage.getItem('eventStorage'))
         retreivedData.push([title, date, time, description, image, eventCreator])
@@ -60,7 +83,7 @@ function newEvent(){
 if (!sessionStorage.getItem('hasVisited')) {
     localStorage.setItem('loggedIn', false)
   console.log("Welcome! This is your very first time here in this session.");
-  
+
   // Set the flag so this block won't run again on reload
   sessionStorage.setItem('hasVisited', 'true');
 } else {
